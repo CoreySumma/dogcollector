d2a572a90c781d27fe77f87c3a4cb2cb3166f6b7
CoreySumma <CoreySumma@gmail.com>
2022-11-16 17:31:58 -0600

    2nd Commit Oops

--- main_app/views.py
+++ main_app/views.py
@@ -1,9 +1,7 @@
 from django.shortcuts import render
+from django.views.generic.edit import CreateView, UpdateView, DeleteView
+from .models import Dog
 
-dogs = [
-  {'Name': 'Milo', 'Breed': 'Husky', 'Description': 'Adventurous and annoying', 'Age': 3},
-  {'Name': 'Hamburger', 'Breed': 'Schnauzer', 'Description': 'Hamburger consumption is unhealthy most of the time', 'Age': 2},
-]
 
 def home(request):
     return render(request, 'home.html')
@@ -12,6 +10,25 @@ def about(request):
     return render(request, 'about.html')
 
 def dogs_index(request):
+    dogs = Dog.objects.all()
     return render(request, 'dogs/index.html', {
         'dogs': dogs
     })
+
+def dogs_detail(request, dog_id):
+    dog = Dog.objects.get(id=dog_id)
+    return render(request, 'dogs/detail.html', {
+        'dog': dog
+    })
+
+class DogCreate(CreateView):
+  model = Dog
+  fields = '__all__'
+
+class DogUpdate(UpdateView):
+    model = Dog
+    fields = ['breed', 'description', 'age']
+
+class DogDelete(DeleteView):
+    model = Dog
+    success_url = '/dogs'
